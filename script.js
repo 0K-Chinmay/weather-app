a2ce258ba6ca9d39e99a7ecd9c56fadcc235ea1a
0K-Chinmay <133075305+0K-Chinmay@users.noreply.github.com>
2023-05-15 18:50:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const error=document.querySelector(".error");
 const details=document.querySelector(".details");
 const search=document.querySelector(".button");
 const tryagain=document.querySelector(".error span");
+const tempo=document.querySelector(".temp");
  search.addEventListener('click', ()=>{
     const city=document.querySelector(".search input").value;
     if(city==''){
@@ -17,6 +18,8 @@ const tryagain=document.querySelector(".error span");
         search_box.style.display='none';
         error.style.display = 'block';
         error.classList.add('fadeIn');
+        tempo.style.display='none';
+        details.style.display='none';
         tryagain.addEventListener('click', ()=>{
             error.classList.remove('fadeIn');
             container.style.height='85px';
@@ -24,8 +27,64 @@ const tryagain=document.querySelector(".error span");
             search_box.style.display='flex';
         })
       }
+      else{
+      tempo.style.display='grid';
+      details.style.display='flex';
+      const weatherimg=document.querySelector(".temp img");
+      const temperature=document.querySelector(".temp .t");
+      const description=document.querySelector(".temp .d");
+      const windspeed=document.querySelector(".wind span");
+      const humid=document.querySelector(".humid span");
+      if(json.weather[0].icon!="01n" ||json.weather[0].icon!="02n"||json.weather[0].icon!="03n"||json.weather[0].icon!="04n"){
+      switch(json.weather[0].main){
+      
+      case 'Clouds':
+          weatherimg.src='SongImg/clouds-and-sun.png';
+          break;
+      case 'Haze':
+          weatherimg.src='SongImg/haze.png';
+          break;
+      case 'Drizzle':
+          weatherimg.src='SongImg/drizzle.png';
+          break;
+      case 'Thunderstorm':
+          weatherimg.src='SongImg/storm.png';
+          break;
+      case 'Clear':
+          weatherimg.src='SongImg/sunny.png';
+          break;
+      case 'Rain':
+          weatherimg.src='SongImg/raining.png';
+          break;
+      case 'Snow':
+          weatherimg.src='SongImg/snow-storm.png';
+           break;
+        }
+      }
+      else{
+        switch(json.weather[0].main){
+      
+      case 'Clear':
+            weatherimg.src='SongImg/half-moon.png';
+            break;
+      default:
+            weatherimg.sec='SongImg/cloudy-moon.png';
+            break;
+        }
+      }
+      temperature.innerHTML=`${parseInt(json.main.temp)}<span>°C</span>`
+      description.innerHTML=`${json.weather[0].description}`
+      windspeed.innerHTML=`${parseInt(json.wind.speed)}Km/h`
+      humid.innerHTML=`${parseInt(json.main.humidity)}%`
+      
+      container.style.height='600px';
+      search_box.style.display='flex';
+      error.style.display = 'none';
+      details.classList.add('fadeIn');
+      tempo.classList.add('fadeIn');
+      
  //add details using json 
-
+    }
 
     })
  })
